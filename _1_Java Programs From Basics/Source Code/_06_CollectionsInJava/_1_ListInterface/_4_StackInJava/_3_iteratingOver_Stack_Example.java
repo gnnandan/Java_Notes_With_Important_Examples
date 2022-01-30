5a918596bee66090649ca69f19aaff3dcc1103b8
Nandan G N <67967990+gnnandan7@users.noreply.github.com>
2022-01-30 23:03:29 +0530

    Vectors And Stack Content Added

---
+++ _1_Java Programs From Basics/Source Code/_06_CollectionsInJava/_1_ListInterface/_4_StackInJava/_3_iteratingOver_Stack_Example.java
@@ -0,0 +1,34 @@
+package _06_CollectionsInJava._1_ListInterface._4_StackInJava;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+public class _3_iteratingOver_Stack_Example
+{
+    public static void main(String[] args)
+    {
+        //creating a stack
+        Stack<String> languages = new Stack<String>();
+
+        //adding elements to stack
+        languages.push("Java");
+        languages.push("Python");
+        languages.push("C");
+        languages.push("C++");
+        languages.push("SQL");
+
+        //way - 1
+        System.out.println("Iterating Over Stack Using 'foreach': ");
+        for(String languageList: languages)
+        {
+            System.out.println(languageList);
+        }
+
+        System.out.println("\nIterating Over Stack Using 'iterator class': ");
+        Iterator<String> languageListIteration = languages.iterator();
+        while(languageListIteration.hasNext())
+        {
+            System.out.println(languageListIteration.next());
+        }
+    }
+}
---
+++ _1_Java Programs From Basics/Source Code/_06_CollectionsInJava/_1_ListInterface/_4_StackInJava/_6_taking_Stack_Input_string_Example.java
@@ -0,0 +1,24 @@
+package _06_CollectionsInJava._1_ListInterface._4_StackInJava;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class _6_taking_Stack_Input_string_Example
+{
+    public static void main(String[] args)
+    {
+        //creating a stack
+        Stack<String> strs = new Stack<String>();
+
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i=0;i<=5;i++)
+        {
+            System.out.print("Enter the element one by one: ");
+            String a = scanner.next();
+            strs.push(a);
+        }
+
+        System.out.println("The final Stack elements are: "+strs);
+    }
+}
