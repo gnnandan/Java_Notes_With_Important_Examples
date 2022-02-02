f62069fa99f6fef4e0acc9ba782e9c6cbdd123cb
Nandan G N <67967990+gnnandan7@users.noreply.github.com>
2022-02-02 10:54:38 +0530

    Array List Content Added

---
+++ _1_Java Programs From Basics/Source Code/_06_CollectionsInJava/_1_List_Interface/_1_ArrayList/_6_arrayList_to_String_Example.java
@@ -0,0 +1,25 @@
+package _06_CollectionsInJava._1_List_Interface._1_ArrayList;
+
+import java.util.ArrayList;
+
+public class _6_arrayList_to_String_Example
+{
+    public static void main(String[] args)
+    {
+        //      Creation Of ArrayList
+        ArrayList<String> languages = new ArrayList<String>();
+
+        //adding elements to arraylist
+        languages.add("java");
+        languages.add("python");
+        languages.add("c#");
+        languages.add("c++");
+        languages.add("c");
+
+        System.out.println("The arraylist elements are: "+languages);
+
+        //converting arraylist to string
+        String list = languages.toString();
+        System.out.println(list);
+    }
+}
---
+++ _1_Java Programs From Basics/Source Code/_06_CollectionsInJava/_1_List_Interface/_4_Stack/_5_taking_Stack_Input_int_Example.java
@@ -0,0 +1,24 @@
+package _06_CollectionsInJava._1_List_Interface._4_Stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class _5_taking_Stack_Input_int_Example
+{
+    public static void main(String[] args)
+    {
+        //creating a stack
+        Stack<Integer> nums = new Stack<Integer>();
+
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i=0;i<=5;i++)
+        {
+            System.out.print("Enter the element one by one: ");
+            Integer a = scanner.nextInt();
+            nums.push(a);
+        }
+
+        System.out.println("The final Stack elements are: "+nums);
+    }
+}
